cc5ebdab10cea8bb20448996878b84ce5bf424a9
FranciscoMaik <chiconunes3081@gmail.com>
2022-09-10 21:32:38 -0300

    feat: create component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React from "react";
+
+import { FirstComponent } from "./components/FirstComponent"
 
 function App() {
+  const name: string = "Maik";
+  const age: number = 26;
+  const isWorking: boolean = true;
+
+  const userGreeting = (name: string): string => {
+    return `Olá, ${name}`;
+  }
+
   return (
     <div className="App">
       <h1>TypeScrip com React</h1>
+      <h2>Nome: {name}</h2>
+      <p>Idade: {age}</p>
+      {isWorking && (
+        <p>Está trabalhando!</p>
+      )}
+
+      <h3>{userGreeting(name)}</h3>
+
+      <FirstComponent />
     </div>
   );
 }
---
+++ src/components/FirstComponent.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export interface IFirstComponentProps {
+}
+
+export function FirstComponent (props: IFirstComponentProps) {
+  return (
+    <div>
+      <p>Primeiro componente</p>
+    </div>
+  );
+}
